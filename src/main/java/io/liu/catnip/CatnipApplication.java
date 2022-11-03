ff772f09d2c89ee4e14aedd34c22179ca1ee6fd7
Deng.Zixu <admin@dengzixu.com>
2022-11-03 22:03:31 +0800

    🎉 初始化项目

---
+++ src/main/java/io/liu/catnip/CatnipApplication.java
@@ -0,0 +1,13 @@
+package io.liu.catnip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatnipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatnipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/liu/catnip/CatnipApplicationTests.java
@@ -0,0 +1,13 @@
+package io.liu.catnip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatnipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
